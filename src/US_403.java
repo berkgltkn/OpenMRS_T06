0e912a43e092c37981d11c6aba6ee365acb03854
Coders Club <hi@codersclub.com>
2025-04-15 00:15:18 +0300

    US_403 created. login page

--- src/US_403.java
+++ src/US_403.java
@@ -1,12 +1,33 @@
 import openMRSUtility.BaseDriver;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class US_403 extends BaseDriver {
+
+    WebDriverWait wait;
+
     @Test
     public void LogoutSystem() {
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
         driver.get("https://demo.openmrs.org/openmrs/login.htm");
+
+        WebElement username = driver.findElement(By.id("username"));
+        wait.until(ExpectedConditions.visibilityOf(username));
+        username.sendKeys("admin");
+
+        WebElement password = driver.findElement(By.id("password"));
+        wait.until(ExpectedConditions.visibilityOf(password));
+        password.sendKeys("Admin123");
+
     }
 }
+
