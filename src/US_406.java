4a6d87d69e9406267f9fc4ffbb9158793e3c6dd6
Baris Sancar <bariss.sancarr@mail.com>
2025-04-17 20:09:21 +0300

    launch browser and navigate to OpenMRS login page

---
+++ src/US_406.java
@@ -0,0 +1,22 @@
+import openMRSUtility.BaseDriver;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.time.Duration;
+
+public class US_406 extends BaseDriver {
+@Test
+public void pattientlist() {
+    WebDriver driver = new ChromeDriver();
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    driver.manage().window().maximize();
+
+    driver.get("https://o2.openmrs.org/openmrs/login.htm");
+}
+}
