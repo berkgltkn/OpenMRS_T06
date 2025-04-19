b9961331650e3df80e0dd210c12993be403ec816
meertsarac <meertsarac@gmail.com>
2025-04-19 18:00:44 +0300

    Added website navigation.

--- src/US_404.java
+++ src/US_404.java
@@ -1,9 +1,13 @@
 import openMRSUtility.BaseDriver;
+import openMRSUtility.patientRegistration_POM;
 import org.junit.Test;
 
 public class US_404 extends BaseDriver {
     @Test
     public void patientRegistration() {
+        patientRegistration_POM register = new patientRegistration_POM();
+
+        driver.get("https://openmrs.org/en/");
 
     }
 }
