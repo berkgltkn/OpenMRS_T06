0b5457189322e7cb57098f75ea803fa844b0a51b
AktsT <akts.tlga@gmail.com>
2025-04-19 19:46:24 +0300

    BaseDriver updated

--- src/openMRSUtility/BaseDriverParameter.java
+++ src/openMRSUtility/BaseDriverParameter.java
@@ -1,22 +1,29 @@
 package openMRSUtility;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Parameters;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 
 public class BaseDriverParameter {
-    public WebDriver driver;
-    public WebDriverWait wait;
+    public static Logger LogTutma = LogManager.getLogger();
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+    public static Actions actionDriver;
 
     @BeforeClass
     @Parameters("BrowserTipi")
@@ -35,10 +42,41 @@ public class BaseDriverParameter {
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        LogTutma.info("Başlangıç değişkenleri driver,wait,log tanımlandı ve başlatıldı");
     }
     @AfterClass
     public void TearDown() {
         MyFunc.wait(3);
         driver.quit();
+        LogTutma.info("Driver kapatıldı");
+        LogTutma.warn("Driver kapatılamadı.");
+    }
+
+    @BeforeMethod
+    public void BeforeMetod() {
+        LogTutma.info("Metod başladı");
+        LogTutma.warn("WARN : Metod başladı, hata oluşmuş olsa idi");
+    }
+
+    @AfterMethod
+    public void AfterMetod(ITestResult sonuc) throws IOException // tesin sonuç ve isim bilgisini olduğu değişkeni
+    {
+        LogTutma.info(sonuc.getName() + ",  Metod Bitti" + " Sonuc=" + (sonuc.getStatus() == 1 ? "Passed" : "Failed"));
+        LogTutma.warn("WARN : Metod bitti, hata oluşmuş olsa idi bu şekilde eklenebilir.Bir if kontrolü ile");
+
+        if(sonuc.getStatus()==ITestResult.FAILURE){
+            TakesScreenshot ts = (TakesScreenshot) driver;
+
+            File source = ts.getScreenshotAs(OutputType.FILE);
+
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            String testMethodName = sonuc.getName();
+
+            String dest = System.getProperty("user.dir") + "/screenshots/" + testMethodName + "_" + timeStamp + ".png";
+            File destination = new File(dest);
+
+            FileUtils.copyFile(source, destination);
+            System.out.println("Screenshot saved: " + dest);
+        }
     }
 }
