81600326fe209c2608a5ef0e8392168a0242679d
meertsarac <meertsarac@gmail.com>
2025-04-19 18:03:52 +0300

    Add explicit wait before clicking demo link

--- src/US_404.java
+++ src/US_404.java
@@ -1,6 +1,8 @@
 import openMRSUtility.BaseDriver;
 import openMRSUtility.patientRegistration_POM;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class US_404 extends BaseDriver {
     @Test
@@ -8,6 +10,9 @@ public class US_404 extends BaseDriver {
         patientRegistration_POM register = new patientRegistration_POM();
 
         driver.get("https://openmrs.org/en/");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[class='zak-button']")));
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("a[class='zak-button']")));
+        register.demoLink.click();
 
     }
 }
